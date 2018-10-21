e3315d29d5f072146d55e8bb52a1ca155a2f1641
covaigroupofconstructions <44133898+covaigroupofconstructions@users.noreply.github.com>
2018-10-21 17:30:31 +0530

    Add files via upload

---
+++ c-projects.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+		<link rel="shortcut icon" href="images/favicon.ico"/>
+		<title>Projects</title>
+
+		<link rel='stylesheet' href='css/bootstrap.min.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/font-awesome.min.css' type='text/css' media='all'/>
+		<link rel='stylesheet' href='css/prettyPhoto.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/owl.carousel.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/style.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/custom.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/preloader.css' type='text/css' media='all' />
+
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900' rel='stylesheet' type='text/css'>
+
+	</head>
+
+	<body class="header01">
+            <?php include'header.php' ?>
+			<div class="site-content">
+				<div class="big-title">
+					<div class="container">
+						<h1 class="entry-title">Completed Projects</h1>
+						<div class="breadcrumb">
+							<div class="container">
+								<ul class="tm_bread_crumb">
+									<li class="top"><a href="index.php">Home</a></li>
+									<li class="sub tail current">Completed Projects</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="our-projects">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="myportfolio-container minimal-light" id="our-projects">
+<div class="our_services_heading3 mt-2 mb-2 text-center">
+									<div>COMPLETED PROJECTS</div>
+								</div>
+	
+									<div class="portfolio-row">
+              							<div class="portfolioContainer portfolioContainer-full row" id="da-thumbs">
+              								<ul class="project-masonry">
+              									<li class="grid-item filter-building our-projects-wrapper col-md-4 col-sm-6 mb-3">
+              										<div class="project-media-cover-wrapper">
+														<div class="project-entry-media">
+															<img src="images/portfolio/new/IMG-20180916-WA0002 (2).jpg" alt="" />
+														</div>
+														<div class="project-entry-cover">
+															<div class="project-overlay our-projects-container"></div>
+															<div class="project-content">
+																<div class="our-projects-categories">
+																	<a href="#">Building</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-title">
+																	<a href="portfolio-detail.html">
+																		White House in London
+																	</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-popup">
+																	<a data-rel="prettyPhoto" href="images/portfolio/portfolio_1_l.jpg">
+																		<i class="fa fa-search"></i>
+																	</a>
+																</div>
+															</div>
+														</div>
+													</div>
+              									</li>
+              									<li class="grid-item filter-3d-design our-projects-wrapper col-md-4 col-sm-6 mb-3">
+													<div class="project-media-cover-wrapper">
+														<div class="project-entry-media">
+															<img src="images/portfolio/new/IMG-20180916-WA0001 (1).jpg" alt="" />
+														</div>
+														<div class="project-entry-cover">
+															<div class="project-overlay our-projects-container"></div>
+															<div class="project-content">
+																<div class="our-projects-categories">
+																	<a href="#">3D Design</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-title">
+																	<a href="portfolio-detail.html">
+																		White House in Singapore
+																	</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-popup">
+																	<a data-rel="prettyPhoto" href="images/portfolio/portfolio_2_l.jpg">
+																		<i class="fa fa-search"></i>
+																	</a>
+																</div>
+															</div>
+														</div>
+													</div>
+												</li>
+												<li class="grid-item filter-exterior our-projects-wrapper col-md-4 col-sm-6 mb-3">
+													<div class="project-media-cover-wrapper">
+														<div class="project-entry-media">
+															<img src="images/portfolio/new/IMG-20180916-WA0000.jpg" alt="" />
+														</div>
+														<div class="project-entry-cover">
+															<div class="project-overlay our-projects-container"></div>
+															<div class="project-content">
+																<div class="our-projects-categories">
+																	<a href="#">Exterior</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-title">
+																	<a href="portfolio-detail.html">
+																		White House in Paris
+																	</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-popup">
+																	<a data-rel="prettyPhoto" href="images/portfolio/portfolio_3_l.jpg">
+																		<i class="fa fa-search"></i>
+																	</a>
+																</div>
+															</div>
+														</div>
+													</div>
+												</li><li class="grid-item filter-building our-projects-wrapper col-md-4 col-sm-6 mb-3">
+              										<div class="project-media-cover-wrapper">
+														<div class="project-entry-media">
+															<img src="images/portfolio/new/IMG-20180916-WA0002 (2).jpg" alt="" />
+														</div>
+														<div class="project-entry-cover">
+															<div class="project-overlay our-projects-container"></div>
+															<div class="project-content">
+																<div class="our-projects-categories">
+																	<a href="#">Building</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-title">
+																	<a href="portfolio-detail.html">
+																		White House in London
+																	</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-popup">
+																	<a data-rel="prettyPhoto" href="images/portfolio/portfolio_1_l.jpg">
+																		<i class="fa fa-search"></i>
+																	</a>
+																</div>
+															</div>
+														</div>
+													</div>
+              									</li>
+              									<li class="grid-item filter-3d-design our-projects-wrapper col-md-4 col-sm-6 mb-3">
+													<div class="project-media-cover-wrapper">
+														<div class="project-entry-media">
+															<img src="images/portfolio/new/IMG-20180916-WA0001 (1).jpg" alt="" />
+														</div>
+														<div class="project-entry-cover">
+															<div class="project-overlay our-projects-container"></div>
+															<div class="project-content">
+																<div class="our-projects-categories">
+																	<a href="#">3D Design</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-title">
+																	<a href="portfolio-detail.html">
+																		White House in Singapore
+																	</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-popup">
+																	<a data-rel="prettyPhoto" href="images/portfolio/portfolio_2_l.jpg">
+																		<i class="fa fa-search"></i>
+																	</a>
+																</div>
+															</div>
+														</div>
+													</div>
+												</li>
+												<li class="grid-item filter-exterior our-projects-wrapper col-md-4 col-sm-6 mb-3">
+													<div class="project-media-cover-wrapper">
+														<div class="project-entry-media">
+															<img src="images/portfolio/new/IMG-20180916-WA0000.jpg" alt="" />
+														</div>
+														<div class="project-entry-cover">
+															<div class="project-overlay our-projects-container"></div>
+															<div class="project-content">
+																<div class="our-projects-categories">
+																	<a href="#">Exterior</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-title">
+																	<a href="portfolio-detail.html">
+																		White House in Paris
+																	</a>
+																</div>
+																<div class="line-clear"></div>
+																<div class="our-projects-popup">
+																	<a data-rel="prettyPhoto" href="images/portfolio/portfolio_3_l.jpg">
+																		<i class="fa fa-search"></i>
+																	</a>
+																</div>
+															</div>
+														</div>
+													</div>
+												</li>
+              								</ul>
+              							</div>
+            						</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+<?php include'footer.php' ?>
+		<a class="scrollup"><i class="fa fa-angle-up"></i></a>
+
+		<script type='text/javascript' src='js/jquery.min.js'></script>
+		<script type='text/javascript' src='js/jquery-migrate.min.js'></script>
+		<script type='text/javascript' src='js/bootstrap.min.js'></script>
+		<script type='text/javascript' src='js/snap.min.js'></script>
+		<script type='text/javascript' src='js/owl.carousel.min.js'></script>
+		<script type='text/javascript' src='js/jquery.prettyPhoto.js'></script>
+		<script type='text/javascript' src='js/jquery.prettyPhoto.init.min.js'></script>
+		<script type='text/javascript' src='js/jQuery.headroom.min.js'></script>
+		<script type='text/javascript' src='js/headroom.min.js'></script>
+		<script type='text/javascript' src='js/script.js'></script>
+		<script type='text/javascript' src="js/modernizr-2.8.3.min.js"></script>
+		<script type='text/javascript' src='js/isotope.pkgd.min.js'></script>
+	    <script type='text/javascript' src='js/imagesloaded.pkgd.js'></script>
+	    <script type='text/javascript' src='js/jquery.hoverdir.js'></script>
+	</body>
+
+<!-- Mirrored from tk-themes.net/html-builder/projects-3-col.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 05 Sep 2018 18:27:04 GMT -->
+</html>
