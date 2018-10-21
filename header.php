dc60a271ee2b264832c46d5f32308ed49c095f2f
covaigroupofconstructions <44133898+covaigroupofconstructions@users.noreply.github.com>
2018-10-21 21:07:28 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,106 @@
+		<div class="snap-drawers">
+			<div class="snap-drawer snap-drawer-left">
+				<div class="mobile-menu">
+					<ul class="menu">
+						<li class="">
+												<a href="index.php">Home</a>
+												<!-- <ul class="sub-menu"> -->
+													<!-- <li><a href="index.html">Home 01</a></li> -->
+													<!-- <li><a href="index-2.html">Home 02</a></li> -->
+													<!-- <li><a href="index-3.html">Home 03</a></li> -->
+												<!-- </ul> -->
+											</li>
+						                    <li class="">
+												<a href="services.php">Services</a>
+												
+											</li>
+											<li class="">
+												<a href="projects.php">Ongoing Projects</a>
+												
+											</li>
+											<li class="">
+												<a href="c-projects.php">Completed Projects</a>
+												
+											</li>
+											
+											<li class="">
+												<a href="contact.php">Contact</a>
+											</li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+		<div id="page" class="site">
+		
+		
+			<div class="top-area site-top hidden-xs hidden-sm">
+				<div class="container">
+					<div class="row middle">
+						<div class="col-md-6">
+							We Build Structures &amp; Relationships to Last
+						</div>
+						<div class="col-md-6 text-right">
+							<div class="top-menu">
+								<ul id="top-menu" class="menu">
+									<!-- <li class="menu-item"><a href="job-search.html">Carrier</a></li> -->
+									<!-- <li class="menu-item"><a href="contact.html">Give Feedback</a></li> -->
+									<!-- <li class="menu-item"><a href="contact-2.html">Contact</a></li> -->
+								</ul>
+							</div>
+							<div class="social-menu">
+								<ul id="social-menu-top" class="menu">
+									<li><a href="https://www.facebook.com/cgc006/">Facebook</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<header class="site-header">
+				<nav id="site-navigation" class="main-navigation">
+					<div class="main-menu">
+						<div class="container">
+							<div class="row middle">
+								<div class="col-md-2 col-xs-8 site-branding">
+									<a href="index.html">
+										<img src="images/logo.png" alt="Logo" class="img-responsive set-logo"/>
+									</a>
+								</div>
+								
+								<div class="col-xs-4 hidden-md hidden-lg end">
+									<i id="open-left" class="fa fa-navicon"></i>
+								</div>
+								<div class="col-md-10 hidden-xs hidden-sm nav-left">
+									<div class="primary-menu">
+										<ul class="menu">
+											<li class="current-menu-item ">
+												<a href="index.html">Home</a>
+												<!-- <ul class="sub-menu"> -->
+													<!-- <li><a href="index.html">Home 01</a></li> -->
+													<!-- <li><a href="index-2.html">Home 02</a></li> -->
+													<!-- <li><a href="index-3.html">Home 03</a></li> -->
+												<!-- </ul> -->
+											</li>
+						                    <li class="">
+												<a href="services.php">Services</a>
+											</li>
+											<li class="">
+												<a href="projects.php">Ongoing Projects</a>
+											</li>
+											<li class="">
+												<a href="c-projects.php">Completed Projects</a>
+											</li>
+											
+											<li class="">
+												<a href="contact.php">Contact</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</nav>
+				<!-- #site-navigation -->
+			</header>
